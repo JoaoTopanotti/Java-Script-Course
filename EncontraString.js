60a37338ce884af1f9536dee00ca58ad11d11056
Joao Vitor Topanotti Barbosa <topanotti09@gmail.com>
2025-04-10 19:52:24 -0400

    Objetos no JS

---
+++ EncontraString.js
@@ -0,0 +1,13 @@
+// Criar um método para ler propriedades de um objeto
+// Exibir somente as propriedades do tipo de string que estão nesse objeto
+
+let objeto = {
+    placaMae : 'ASUS Tuf Gaming B550',
+    processador : 'Ryzen 5700X',
+    ram : 32,
+    placaVideo : 'NVDIA RTX 4060 Ti'
+}
+console.log(objeto.processador);
+console.log(objeto.placaMae);
+console.log(objeto.ram + 'Gb');
+console.log(objeto.placaVideo);
